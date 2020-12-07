762fcce87593a476ad9f5564042d2198876f2c6c
nvasiliev <nikita.postpunk@gmail.com>
2020-12-07 03:25:16 +0300

    Refactor day 5

--- _2020/day_5/task1.py
+++ _2020/day_5/task1.py
@@ -1,28 +1,8 @@
-from typing import Tuple
+import re
 
 
-def decode_boarding_pass(boarding_pass: str) -> Tuple[int, int]:
-    def bin_search(s: str, *, left_char: str, start: int, range_: Tuple[int, int]):
-        left, right = range_
-        i = start
-
-        while left < right:
-            c = s[i]
-            middle = (right - left) // 2 + left
-
-            if c == left_char:
-                right = middle
-            else:
-                left = middle + 1
-
-            i += 1
-
-        return left, i
-
-    row, i = bin_search(boarding_pass, start = 0, left_char = 'F', range_ = (0, 127))
-    column, _ = bin_search(boarding_pass, start = i, left_char = 'L', range_ = (0, 7))
-
-    return row * 8 + column
+def decode_boarding_pass(boarding_pass: str) -> int:
+    return int(re.sub(r'B|R', '1', re.sub(r'F|L', '0', boarding_pass)), 2)
 
 
 def main(input_path = 0):
