913edae8189ad5d808ea9b8d4c8c44d49da19a59
nvasiliev <nikita.postpunk@gmail.com>
2020-12-12 21:22:56 +0300

    Solve day 7 part 1

---
+++ _2020/day_7/task1.py
@@ -0,0 +1,37 @@
+from typing import Dict, List
+
+
+def parse_neighbor(raw_neighbor: str) -> str:
+    pieces = raw_neighbor.split(' ')
+    return f'{pieces[1]} {pieces[2]}'
+
+
+def dfs(g: Dict[str, List[str]], target: str, u: str) -> bool:
+    if u == target:
+        return True
+
+    for v in g[u]:
+        if dfs(g, target, v):
+            return True
+
+    return False
+
+
+def main(input_path = 0):
+    with open(input_path) as f:
+        target = 'shiny gold'
+        empty = 'no other bags.'
+
+        g = {}
+
+        for line in f:
+            line = line.strip()
+            u, v = line.split(' bags contain ')
+            g[u] = [] if v == empty else [parse_neighbor(x) for x in v.split(', ')]
+
+        count = sum(dfs(g, target, u) for u in g if u != target)
+        print(count)
+
+
+if __name__ == '__main__':
+    main()
